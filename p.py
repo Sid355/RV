c120264223aec5802bd17fd91cc6c4ce09ab4db6
Sidhartha <sidharthpatnaik96@gmail.com>
2024-11-05 18:49:45 +0530

    -

--- p.py
+++ p.py
@@ -23,4 +23,47 @@ def d():
     }
     response = requests.get(url, headers=headers)
     open('mcx.txt','w').write(response.text)
-d()
+def t():
+    import json
+    from pprint import pprint
+    nl=open('mcx.txt','r').read()
+    nl=json.loads(nl)['data']['candles']
+    ns=[]
+    pks=[]
+    vps=[]
+    for n in range(0,len(nl)):
+        k=[int(s) for s in nl[n][0].split('T')[1].split('+')[0].split(':')]
+        if k[0]==9 and k[1]<=30:
+            pks+=[nl[n][1]]
+        elif pks!=[]:
+            ns+=[pks]
+            vps+=[nl[n+30][1]]
+            pks=[]
+    for n in range(0,len(ns)):
+        s=0
+        for ps in range(0,len(ns[n])):
+            s+=ns[n][ps]
+        s/=len(ns[n])
+        vps[n]=(vps[n]-ns[n][-1])/ns[n][-1]
+        for ps in range(0,len(ns[n])):
+            ns[n][ps]=(ns[n][ps]-s)/s
+    from sklearn.cluster import KMeans
+    import numpy as np
+    ns=np.array(ns)
+    vss=5
+    vk=KMeans(n_clusters=vss,random_state=0,n_init="auto").fit(ns)
+    vks=vk.predict(ns)
+    svvs=[0]*vss
+    pvs=[0]*vss
+    for n in range(0,len(vks)):
+        svvs[vks[n]]+=vps[n]/vss
+        pvs[vks[n]]+=1
+    print(svvs,pvs)
+    import matplotlib.pyplot as plt
+    for k in range(0,vss):
+        plt.figure(k)
+        for n in range(0,len(vks)):
+            if vks[n]==k:
+                plt.plot(ns[n])
+    plt.show()
+t()
